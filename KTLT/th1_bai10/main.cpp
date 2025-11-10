9ce4c3c4ddfe100246e5e3bcd81fe7dc59994c85
chongkhai2006-cpu <chongkhai2006@gmail.com>
2025-11-10 19:34:33 +0700

    update

--- KTLT/th1_bai10/main.cpp
+++ KTLT/th1_bai10/main.cpp
@@ -1,9 +1,16 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
-
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+double tiencanco, laisuat, tiengui;
+int thang;
+cout<<"Nhap tien can co:";
+cin>>tiencanco;
+cout<<"Nhap lai suat:";
+cin>>laisuat;
+cout<<"So thang can gui:";
+cin>>thang;
+tiengui=tiencanco/pow(1+laisuat,thang);
+cout << "So tien it nhat phai gui ban dau la: " << tiengui;
+return 0;
 }
